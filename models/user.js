554e16bbf19aa1c1b4cdebf392dce52f69a8588c
gautampanchal94 <gautampanchal01@gmail.com>
2022-01-26 14:45:21 +0530

    Compelete v1

--- models/user.js
+++ models/user.js
@@ -6,6 +6,38 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  homeContent: {
+    pageTitle: {
+      title: {
+        type: String,
+        default: "Puchinda Chai",
+      },
+      content: {
+        type: String,
+        default: "It's you, you idiot.",
+      },
+    },
+    firstSubHeading: {
+      title: {
+        type: String,
+        default: "Project mission",
+      },
+      content: {
+        type: String,
+        default: "To only let you know what you already know",
+      },
+    },
+    lastSubHeading: {
+      title: {
+        type: String,
+        default: "Want to Say Something?",
+      },
+      content: {
+        type: String,
+        default: "Call Karna",
+      },
+    },
+  },
 });
 
 userSchema.plugin(passportLocalMongoose);
--- routes/default-routes.js
+++ routes/default-routes.js
@@ -1,11 +1,66 @@
 const express = require("express");
 
+const { t } = require("typy");
+
+const User = require("../models/user");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  req.isAuthenticated()
-    ? res.render("home", { isLoggedIn: true })
-    : res.render("home", { isLoggedIn: false });
+router.get("/", function (req, res) {
+  if (req.isAuthenticated()) {
+    User.find({ _id: req.user._id }, function (err, content) {
+      if (!err) {
+        res.render("home", { isLoggedIn: true, homeContent: content });
+      } else {
+        console.log(err);
+      }
+    });
+  } else {
+    res.render("home", { isLoggedIn: false, homeContent: "" });
+  }
+});
+
+router.get("/update-home", function (req, res) {
+  if (req.isAuthenticated()) {
+    User.find({ _id: req.user._id }, function (err, content) {
+      if (!err) {
+        res.render("update-home", { homeContent: content });
+      } else {
+        console.log(err);
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.post("/update-home", async function (req, res) {
+  if (req.isAuthenticated()) {
+    await User.findOneAndUpdate(
+      { _id: req.user._id },
+      {
+        $set: {
+          homeContent: {
+            pageTitle: {
+              title: req.body.pageTitle,
+              content: req.body.pageContent,
+            },
+            firstSubHeading: {
+              title: req.body.headFirst,
+              content: req.body.bodyFirst,
+            },
+            lastSubHeading: {
+              title: req.body.headLast,
+              content: req.body.bodyLast,
+            },
+          },
+        },
+      }
+    );
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 module.exports = router;
