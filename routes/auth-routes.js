d0e3f5745a7f6f720f18612ed828cd6714f04496
gautampanchal94 <gautampanchal01@gmail.com>
2022-01-27 11:51:27 +0530

    Register page deleted

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,8 +3,6 @@ const express = require("express");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
-const validator = require("email-validator");
-const passwordValidator = require("password-validator");
 
 const User = require("../models/user");
 
@@ -33,63 +31,6 @@ router.post(
 router.get("/logout", function (req, res) {
   req.logout();
   res.redirect("/");
-});
-
-router.get("/register", (req, res) => {
-  if (!req.isAuthenticated()) {
-    res.render("register", {
-      messages: false,
-      registeredAlert: req.flash("validMessage"),
-    });
-  } else {
-    res.redirect("/");
-  }
-});
-
-router.post("/register", (req, res) => {
-  // password validator
-  const passwordSchema = new passwordValidator();
-  passwordSchema
-    .min(6, "Minimum 6 character long.")
-    .max(20, "Maximum 20 character allowed.")
-    .uppercase(1, "At least 1 uppercase letters.")
-    .symbols(1, "Must include at least 1 special character.")
-    .digits(1, "Must include at least 1 digit.")
-    .not()
-    .spaces(0, "No Spaces allowed.");
-
-  // authentic
-  if (validator.validate(req.body.username)) {
-    //password check
-    if (!passwordSchema.validate(req.body.password)) {
-      res.render("register", {
-        messages: passwordSchema.validate(req.body.password, {
-          details: true,
-        }),
-        registeredAlert: false,
-      });
-    } else {
-      User.register(
-        { name: req.body.name, username: req.body.username },
-        req.body.password,
-        function (err, user) {
-          if (err) {
-            // console.log("Registered");
-            req.flash("validMessage", "User is already registered.");
-            res.redirect("/register");
-          } else {
-            passport.authenticate("local")(req, res, () => {
-              res.redirect("/");
-            });
-          }
-        }
-      );
-    }
-  } else {
-    // console.log("Invalid Email!!");
-    req.flash("validMessage", "Enter valid email address.");
-    res.redirect("/register");
-  }
 });
 
 module.exports = router;
