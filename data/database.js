00f4622bc8e97c18cc4fb78f1e0cc95e9fea9b77
gautampanchal94 <gautampanchal01@gmail.com>
2022-01-27 17:29:26 +0530

    DB change

--- data/database.js
+++ data/database.js
@@ -1,8 +1,6 @@
 //jshint esversion:6
 const mongoose = require("mongoose");
 
-let db = "mongodb://localhost:27017/myDB";
-
 async function connectToDatabase() {
   if (process.env.DATABASE) {
     db = process.env.DATABASE;
