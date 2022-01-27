e698a2e94dac340d7715ea4aa213fac41a8648ab
gautampanchal94 <gautampanchal01@gmail.com>
2022-01-27 17:43:39 +0530

    Database Edit

--- data/database.js
+++ data/database.js
@@ -1,10 +1,8 @@
 //jshint esversion:6
 const mongoose = require("mongoose");
 
-async function connectToDatabase() {
-  if (process.env.DATABASE) {
-    db = process.env.DATABASE;
-  }
+function connectToDatabase() {
+  db = process.env.DATABASE;
   mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch((error) => console.log(error));
